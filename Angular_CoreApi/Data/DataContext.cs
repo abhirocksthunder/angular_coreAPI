2864c6da03983d7174589b3692d7543ac948b410
Abhilash <abhilash.somisetty@gmail.com>
2020-07-30 23:59:47 +0530

    Initial Commit

---
+++ Angular_CoreApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Angular_CoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angular_CoreApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Angular_CoreApi/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace Angular_CoreApi.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
